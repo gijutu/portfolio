95f17f92e5506b0d409601a3c70f53110b695971
gijutu <31819030+gijutu@users.noreply.github.com>
2019-11-01 19:31:01 +0900

    Fix docs rules for vue files

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="welcome">
-    <h1>{{ msg }}</h1>
-    <router-link to="/">ホーム</router-link>
-    <router-link to="/profile">プロフィール</router-link>
-    <router-link to="/works">制作物</router-link>
-  </div>
+<div class = "welcome">
+  <h1>{{ msg }}</h1>
+  <router-link to="/">ホーム</router-link>
+  <router-link to="/profile">プロフィール</router-link>
+  <router-link to="/works">制作物</router-link>
+</div>
 </template>
 
 <script>
-  export default {
-    name: 'welcome',
-    data () {
-      return {
-        msg: 'ホームページ'
-      }
+export default {
+  name: 'welcome',
+  data () {
+    return {
+      msg: 'ホームページ'
     }
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
+h1, h2 {
+  font-weight: normal;
+}
 
-  a {
-    color: #42b983;
-  }
+a {
+  color: #42b983;
+}
 </style>
